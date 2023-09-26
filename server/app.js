e26a13653afc99225acd6478a5a5c379a616b134
Ankita Malik <ankitamalik936@gmail.com>
2023-09-26 12:31:23 +0530

    modified

--- server/app.js
+++ server/app.js
@@ -34,11 +34,11 @@ app.use('/api/product', productRoutes);
 
 // =============================================================================================================================== //
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+// });
 
 
 
